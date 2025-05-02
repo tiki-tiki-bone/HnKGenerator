b638f804b13f59010a6562898656d948615e5be6
tiki-tiki-bone <tikinaga@yahoo.co.jp>
2025-05-03 00:41:38 +0900

    items.js仮置き

---
+++ js/items.js
@@ -0,0 +1,14 @@
+// 表示項目名とアドレスのマッピング
+const items = {
+    "": null,
+    "1Pブースト": 0x00ab0dd4,
+    "2Pブースト": 0x00ab0dc2,
+    "1Pガークラゲージ": 0x00aaed7c,
+    "2Pガークラゲージ": 0x00ab0d9c,
+    "1P気絶値": 0x00aaedb4,
+    "2P気絶値": 0x00ab0dd4,
+    "1P気絶耐久値": 0x00aaedba,
+    "2P気絶耐久値": 0x00ab0dda,
+};
+
+export default items;
